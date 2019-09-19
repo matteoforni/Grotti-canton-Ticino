fcfcf077d3a2e3ff302979d0ac1379f8fceff5ae
Matteo Forni <37305596+matteoforni@users.noreply.github.com>
2019-09-19 14:22:17 +0200

    add map

--- codice/application/views/home/index.php
+++ codice/application/views/home/index.php
@@ -1,7 +1,18 @@
-<head>
-    <title>Grotti cantone Ticino</title>
-</head>
+    <div id="map">
 
-<body>
-    <h1>Home page</h1>
-</body>
+    </div>
+    <script>
+        function initMap() {
+            // The location of Uluru
+            var uluru = {lat: -25.344, lng: 131.036};
+            // The map, centered at Uluru
+            var map = new google.maps.Map(
+                document.getElementById('map'), {zoom: 4, center: uluru});
+            // The marker, positioned at Uluru
+            var marker = new google.maps.Marker({position: uluru, map: map});
+        }
+    </script>
+
+    <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCCOSBrPiB40uF9Oee8IxwUdxoIZu_9XBg&callback=initMap">
+    </script>
