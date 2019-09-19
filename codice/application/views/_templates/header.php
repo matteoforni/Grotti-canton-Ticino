39bbc2a9c4cd81de5eaa25adadeca64eb90091b3
Matteo Forni <37305596+matteoforni@users.noreply.github.com>
2019-09-19 14:21:52 +0200

    create header

--- codice/application/views/_templates/header.php
+++ codice/application/views/_templates/header.php
@@ -23,8 +23,30 @@
 </head>
 
 <body>
-
-</body>
+<header>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light grey lighten-3 scrolling-navbar">
+        <a class="navbar-brand" href="#"><strong>Grotti Ticinesi</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="justify-content-end collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Opinions</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
 
 <!-- JQuery -->
 <script type="text/javascript" src="./application/assets/mdb/js/jquery-3.4.1.min.js"></script>
