f5797bb8103447bbd562e1d78895e05fc5f330f4
Your Name <matteoforni@hotmail.com>
2019-10-10 16:01:52 +0200

    aggiunto possibilita di votare

--- codice/application/controller/grotto.php
+++ codice/application/controller/grotto.php
@@ -9,7 +9,6 @@ class Grotto
 
         if(isset($_SESSION['user'])){
             //mostro l'index per gli utenti loggati
-
             if($_SESSION['user']['nome_ruolo'] == 'admin'){
                 ViewLoader::load("_templates/header_admin");
                 ViewLoader::load("grotto/index");
@@ -27,6 +26,7 @@ class Grotto
         }
     }
 
+    //funzione che mostra il grotto scelto dalla tabella
     public function show($id){
         require_once "./application/models/db_connection.php";
         require_once "./application/models/input_manager.php";
@@ -38,6 +38,7 @@ class Grotto
             unset($_SESSION['img']);
         }
 
+        //Carico dal DB il grotto e le sue immagini
         $im = new InputManager();
         $id = filter_var($im->checkInput($id), FILTER_SANITIZE_NUMBER_INT);
         $grotto = (new db_connection)->getGrotto($id);
@@ -45,5 +46,37 @@ class Grotto
         $_SESSION['grotto'] = $grotto;
         $_SESSION['img'] = $images;
         header('Location: ' . URL . 'grotto');
+    }
+
+    //Funzione che consente di votare un grotto
+    public function vota(){
+        require_once "./application/models/db_connection.php";
+        require_once "./application/models/input_manager.php";
+        $im = new InputManager();
+        $errors = Array();
+        unset($_SESSION['errors']);
+
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            if(isset($_POST['val']) && !empty($_POST['val']) && isset($_SESSION['user'])){
+
+                //Prendo i dati dell'utente, del grotto e la votazione
+                $grotto = $_SESSION['grotto']['id'];
+                $utente = $_SESSION['user']['email'];
+                $valutazione = filter_var($im->checkInput($_POST['val']), FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+
+                if($valutazione >= 0 && $valutazione <= 5) {
+                    //Inserisco il voto nel DB
+                    (new db_connection())->addVoto($grotto, $utente, $valutazione);
+                    header('Location: ' . URL . 'grotto/show/' . $grotto['id']);
+                    exit();
+                }else{
+                    //se non vengono inseriti i dati in maniera corretta
+                    array_push($errors, "Inserire una votazione valida");
+                    $_SESSION['errors'] = $errors;
+                    header('Location: ' . URL . 'grotto/show/' . $grotto['id']);
+                    exit();
+                }
+            }
+        }
     }
 }
--- codice/application/views/grotto/index.php
+++ codice/application/views/grotto/index.php
@@ -1,59 +1,104 @@
 <?php
 if(isset($_SESSION['grotto']) && isset($_SESSION['img'])): ?>
-<?php
-$grotto = $_SESSION['grotto'];
-$img = $_SESSION['img'];
-$i = 0;
-?>
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="my-5">
-                <h1 class="h1 text-center"><?php echo $grotto['nome']; ?></h1>
+    <?php
+    $grotto = $_SESSION['grotto'];
+    $img = $_SESSION['img'];
+    $i = 0;
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
                 <div class="my-5">
-                    <!-- CAROUSEL -->
-                    <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
-                        <div class="carousel-inner">
-                            <?php foreach ($img as $item): ?>
-                                <?php if($i == 0): ?>
-                                    <div class="carousel-item active">
-                                        <img class="d-block w-100" src="<?php echo $item['path']; ?>" alt="<?php echo $item['titolo']; ?>">
-                                    </div>
-                                <?php else: ?>
-                                    <div class="carousel-item">
-                                        <img class="d-block w-100" src="<?php echo $item['path']; ?>" alt="<?php echo $item['titolo']; ?>">
-                                    </div>
-                                <?php endif; ?>
-                            <?php $i++; ?>
-                            <?php endforeach; ?>
+                    <h1 class="h1 text-center"><?php echo $grotto['nome']; ?></h1>
+                    <div class="my-5">
+                        <!-- CAROUSEL -->
+                        <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <?php foreach ($img as $item): ?>
+                                    <?php if($i == 0): ?>
+                                        <div class="carousel-item active">
+                                            <img class="d-block w-100" src="<?php echo $item['path']; ?>" alt="<?php echo $item['titolo']; ?>">
+                                        </div>
+                                    <?php else: ?>
+                                        <div class="carousel-item">
+                                            <img class="d-block w-100" src="<?php echo $item['path']; ?>" alt="<?php echo $item['titolo']; ?>">
+                                        </div>
+                                    <?php endif; ?>
+                                    <?php $i++; ?>
+                                <?php endforeach; ?>
+                            </div>
+                            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
                         </div>
-                        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
                     </div>
-                </div>
-                <div class="my-5">
-                    <h5 class="h5">Contatti</h5>
-                    <p class="ml-2">Telefono: <?php echo $grotto['telefono'] ?></p>
-                </div>
-                <div class="my-5">
-                    <h5 class="h5">Dove siamo</h5>
-                    <p class="ml-2">
-                        Indirizzo: <?php echo $grotto['cap'] . " " . $grotto['paese'] . " " . $grotto['via'] . " " . $grotto['no_civico']; ?>
-                    </p>
-                </div>
-                <div class="my-5">
-                    <h5 class="h5">Valutazioni</h5>
-                    <p class="ml-2">Fascia di prezzo: <?php echo $grotto['fascia_prezzo']; ?><br>
-                    Valutazione: <?php echo $grotto['valutazione']; ?></p>
+                    <div class="my-5">
+                        <h5 class="h5">Contatti</h5>
+                        <p class="ml-2">Telefono: <?php echo $grotto['telefono'] ?></p>
+                    </div>
+                    <div class="my-5">
+                        <h5 class="h5">Dove siamo</h5>
+                        <p class="ml-2">
+                            Indirizzo: <?php echo $grotto['cap'] . " " . $grotto['paese'] . " " . $grotto['via'] . " " . $grotto['no_civico']; ?>
+                        </p>
+                    </div>
+                    <div class="my-5">
+                        <h5 class="h5">Valutazioni</h5>
+                        <p class="ml-2">Fascia di prezzo: <?php echo $grotto['fascia_prezzo']; ?><br>
+                            Valutazione: <?php echo $grotto['valutazione']; ?></p>
+                    </div>
+                    <div class="my-5 col-md-6 offset-md-3">
+                        <form method="post" class="text-center border border-light p-5" action="<?php URL ?>grotto/vota">
+                            <p class="h4 mb-4">Vota</p>
+                            <input type="hidden" id="val" name="val" value="" />
+                            <div class="rating-container">
+                                <div id="valutazione" name="valutazione" class="rating"></div>
+                            </div>
+                            <input class="btn btn-info mt-4" type="submit" id="submit">
+                            <?php
+                            if(isset($_SESSION['errors'])){
+                                foreach ($_SESSION['errors'] as $item) {
+                                    echo "<p class='text-danger'>" . $item . "</p><br>";
+                                }
+                            }
+                            ?>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <script>
+        $(document).ready(function () {
+
+            //Imposto le opzioni
+            var options = {
+                max_value: 5,
+                step_size: 0.5,
+            };
+
+            //Istanzio la valutazione
+            $(".rating").rate(options);
+
+            //Ad ogni click setto come value dell'input nascosto val il valore della valutazione
+            $(".rating").click(function () {
+                var value = String($(".rating").rate("getValue"));
+                $('#val').val(value);
+                console.log(value);
+            });
+
+            //Mantengo sempre centrate le stelle
+            var margin = $('.rating-container').width()/2 - $('.rating').width()/2;
+            $(".rating").css('margin-left', margin);
+            $(window).resize(function () {
+                var margin = $('.rating-container').width()/2 - $('.rating').width()/2;
+                $(".rating").css('margin-left', margin);
+            });
+        });
+    </script>
 <?php endif; ?>
