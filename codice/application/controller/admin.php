a4ad8ce0c2ef9fc177e5e54b9a22226245bfaebe
Your Name <matteoforni@hotmail.com>
2019-10-17 16:13:38 +0200

    update admin page

--- codice/application/controller/admin.php
+++ codice/application/controller/admin.php
@@ -5,9 +5,80 @@ class Admin
 {
     public function index()
     {
-        //Show index page
-        ViewLoader::load("_templates/header_admin");
-        ViewLoader::load("admin/index");
-        ViewLoader::load("_templates/footer");
+        if(isset($_SESSION['user'])){
+            //mostro l'index per gli utenti loggati
+            if($_SESSION['user']['nome_ruolo'] == 'admin'){
+                require_once "./application/models/DBConnection.php";
+
+                //Carico gli utenti
+                $utenti = (new DBConnection)->getUsers();
+                $_SESSION['users'] = $utenti;
+
+                //Carico i grotti già validati
+                $grotti_validati = (new DBConnection)->getGrotti(true);
+                $_SESSION['grotti_validati'] = $grotti_validati;
+
+                //Carico i grotti da validare
+                $grotti = (new DBConnection)->getGrotti(false);
+                $_SESSION['grotti'] = $grotti;
+
+                ViewLoader::load("_templates/header_admin");
+                ViewLoader::load("admin/index");
+                ViewLoader::load("_templates/footer");
+            }elseif($_SESSION['user']['nome_ruolo'] == 'utente'){
+                ViewLoader::load("_templates/header_user");
+                ViewLoader::load("home/index");
+                ViewLoader::load("_templates/footer");
+            }
+        }else{
+            //mostro l'index di base
+            ViewLoader::load("_templates/header_base");
+            ViewLoader::load("home/index");
+            ViewLoader::load("_templates/footer");
+        }
+    }
+
+    public function showUser($email){
+        require_once "./application/models/DBConnection.php";
+        require_once "./application/models/input_manager.php";
+
+        if(isset($_SESSION['user'])) {
+            if ($_SESSION['user']['nome_ruolo'] == 'admin') {
+                if(isset($_SESSION['utenti'])){
+                    if(isset($_SESSION['utente'])){
+                        unset($_SESSION['utente']);
+                    }
+
+                    //Carico dal DB l'utente
+                    $im = new InputManager();
+                    $email = filter_var($im->checkInput($email), FILTER_SANITIZE_EMAIL);
+                    $utente = (new DBConnection)->getUser($email);
+                    $_SESSION['utente'] = $utente;
+                    header('Location: ' . URL . '');
+                }
+            }
+        }
+    }
+
+    public function showGrotto($id){
+        require_once "./application/models/DBConnection.php";
+        require_once "./application/models/input_manager.php";
+
+        if(isset($_SESSION['user'])) {
+            if ($_SESSION['user']['nome_ruolo'] == 'admin') {
+                if(isset($_SESSION['grotti_validati'])){
+                    if(isset($_SESSION['grotto'])){
+                        unset($_SESSION['grotto']);
+                    }
+
+                    //Carico dal DB il grotto
+                    $im = new InputManager();
+                    $id = filter_var($im->checkInput($id), FILTER_SANITIZE_NUMBER_INT);
+                    $grotto = (new DBConnection)->getGrotto($id);
+                    $_SESSION['grotto'] = $grotto;
+                    header('Location: ' . URL . '');
+                }
+            }
+        }
     }
 }
--- codice/application/views/admin/index.php
+++ codice/application/views/admin/index.php
@@ -1,5 +1,174 @@
-<h1>Sei un admin!!!</h1>
-<?php
-echo "<pre>";
-print_r($_SESSION['user']);
-echo "</pre>";
+<?php if(isset($_SESSION['user']) && $_SESSION['user']['nome_ruolo'] == 'admin' && isset($_SESSION['grotti']) && isset($_SESSION['grotti_validati']) && isset($_SESSION['users'])): ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="my-5 text-center">
+                    <h1 class="h1 d-block">Amministrazione grotti Ticino</h1>
+                    <h6 class="h6">Connesso come: <?php echo $_SESSION['user']['nome'] . " " . $_SESSION['user']['cognome'] . " (" .$_SESSION['user']['username'] . ")" ?></h6>
+
+                    <!-- Sezione di gestione degli utenti -->
+                    <div class="my-5">
+                        <h3 class="h3-responsive">Gestione utenti</h3>
+                        <input type="button" class="btn btn-info" value="Gestisci utenti" id="utenti">
+
+                        <table id="utenti_table" class="table table-hover table-responsive-md">
+                            <thead>
+                            <tr class="text-center">
+                                <th scope="col">Email</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Cognome</th>
+                                <th scope="col">Username</th>
+                                <th scope="col">Ruolo</th>
+                                <th scope="col">Modifica</th>
+                                <th scope="col">Elimina</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($_SESSION['users'] as $row): ?>
+                                <tr class="text-center table-row" data-href="<?php echo URL; ?>admin/showUser/<?php echo $row['email']; ?>">
+                                    <td><?php echo $row['email']; ?></td>
+                                    <td><?php echo $row['nome']; ?></td>
+                                    <td><?php echo $row['cognome']; ?></td>
+                                    <td><?php echo $row['username']; ?></td>
+                                    <td><?php echo $row['nome_ruolo']; ?></td>
+                                    <td><button type="button" class="btn btn-sm btn-warning">Modifica</button></td>
+                                    <td><button type="button" class="btn btn-sm btn-danger">Elimina</button></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <!-- Sezione di gestione dei grotti -->
+                    <div class="my-5">
+                        <h3 class="h3-responsive">Gestione Grotti</h3>
+                        <input type="button" class="btn btn-info" value="Gestisci Grotti" id="grotti">
+
+                        <table id="grotti_table" class="table table-hover table-responsive-md">
+                            <thead>
+                            <tr class="text-center">
+                                <th scope="col">Nome</th>
+                                <th scope="col">Indirizzo</th>
+                                <th scope="col">Telefono</th>
+                                <th scope="col">Fascia di Prezzo</th>
+                                <th scope="col">Valutazione</th>
+                                <th scope="col">Modifica</th>
+                                <th scope="col">Elimina</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($_SESSION['grotti_validati'] as $row): ?>
+                                <tr class="text-center table-row" data-href="<?php echo URL; ?>admin/showGrotto/<?php echo $row['id']; ?>">
+                                    <td><?php echo $row['nome']; ?></td>
+                                    <td><?php echo($row['cap'] . " " . $row['paese'] . ", " .$row['via'] . " " . $row['no_civico']); ?></td>
+                                    <td><?php echo $row['telefono']; ?></td>
+                                    <td><?php echo $row['fascia_prezzo']; ?></td>
+                                    <td>
+                                        <p class="hidden"><?php echo $row['valutazione']; ?></p>
+                                        <div class="rating-container-small">
+                                            <div id="valutazione" class="rating" data-rate-value=<?php echo $row['valutazione']; ?>></div>
+                                        </div>
+                                    </td>
+                                    <td><button type="button" class="btn btn-sm btn-warning">Modifica</button></td>
+                                    <td><button type="button" class="btn btn-sm btn-danger">Elimina</button></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <!-- Sezione di gestione degli inserimenti -->
+                    <div class="my-5">
+                        <h3 class="h3-responsive">Gestione Inserimenti</h3>
+                        <input type="button" class="btn btn-info" value="Gestisci inserimenti" id="inserimenti">
+
+                        <table id="ins_table" class="table table-hover table-responsive-md">
+                            <thead>
+                                <tr class="text-center">
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Indirizzo</th>
+                                    <th scope="col">Telefono</th>
+                                    <th scope="col">Fascia di Prezzo</th>
+                                    <th scope="col">Accetta</th>
+                                    <th scope="col">Elimina</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($_SESSION['grotti'] as $row): ?>
+                                <tr class="text-center table-row" data-href="<?php echo URL; ?>admin/showGrotto/<?php echo $row['id']; ?>">
+                                    <td><?php echo $row['nome']; ?></td>
+                                    <td><?php echo($row['cap'] . " " . $row['paese'] . ", " .$row['via'] . " " . $row['no_civico']); ?></td>
+                                    <td><?php echo $row['telefono']; ?></td>
+                                    <td><?php echo $row['fascia_prezzo']; ?></td>
+                                    <td><button type="button" class="btn btn-sm btn-success">Accetta</button></td>
+                                    <td><button type="button" class="btn btn-sm btn-danger">Elimina</button></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function () {
+            //Rendo le righe delle tabelle cliccabili grazie all'attributo data-href
+            $(".table-row").click(function() {
+                window.location = $(this).data("href");
+            });
+
+            //Imposto le opzioni della valutazione
+            var options = {
+                max_value: 5,
+                step_size: 0.5,
+                readonly: true,
+            };
+
+            //Istanzio la valutazione
+            $(".rating").rate(options);
+
+            //Mantengo sempre centrate le stelle
+            var margin = $('.rating-container-small').width()/2 - $('.rating').width()/2;
+            $(".rating").css('margin-left', margin);
+            $(window).resize(function () {
+                var margin = $('.rating-container-small').width()/2 - $('.rating').width()/2;
+                $(".rating").css('margin-left', margin);
+            });
+
+            //Nascondo le tabelle
+            $('#utenti_table').hide();
+            $('#grotti_table').hide();
+            $('#ins_table').hide();
+
+            //Al click del bottone utenti mostro la tabella relativa
+            $('#utenti').click(function () {
+                if($("#utenti_table").is(":visible")) {
+                    $('#utenti_table').hide();
+                    $('#utenti_table').find('thead').eq(1).hide();
+                }else {
+                    $('#utenti_table').show();
+                }
+            });
+
+            //Al click del bottone grotti mostro la tabella relativa
+            $('#grotti').click(function () {
+                if($("#grotti_table").is(":visible")) {
+                    $('#grotti_table').hide();
+                }else {
+                    $('#grotti_table').show();
+                }
+            });
+
+            //Al click del bottone inserimenti mostro la tabella relativa
+            $('#inserimenti').click(function () {
+                if($("#ins_table").is(":visible")) {
+                    $('#ins_table').hide();
+                }else {
+                    $('#ins_table').show();
+                }
+            });
+        });
+    </script>
+<?php endif; ?>
