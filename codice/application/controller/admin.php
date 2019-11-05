d17f1b1067ba2dd34c8d5cb4b4867c1d33ed13c5
Your Name <matteoforni@hotmail.com>
2019-11-05 16:21:23 +0100

    add images management table

--- codice/application/controller/admin.php
+++ codice/application/controller/admin.php
@@ -3,68 +3,97 @@
 
 class Admin
 {
+    /**
+     * Funzione che mostra la pagina di admin solo se si è connessi e se il proprio utente è un admin.
+     */
     public function index()
     {
         if(isset($_SESSION['user'])){
-            //mostro l'index per gli utenti loggati
+            //mostro l'index per gli utenti loggati.
             if($_SESSION['user']['nome_ruolo'] == 'admin'){
                 require_once "./application/models/DBConnection.php";
 
-                //Carico gli utenti
+                //Carico gli utenti.
                 $utenti = (new DBConnection)->getUsers();
                 $_SESSION['users'] = $utenti;
 
-                //Carico i grotti già validati
+                //Carico i grotti già validati.
                 $grotti_validati = (new DBConnection)->getGrotti(true);
                 $_SESSION['grotti_validati'] = $grotti_validati;
 
-                //Carico i grotti da validare
+                //Carico i grotti da validare.
                 $grotti = (new DBConnection)->getGrotti(false);
                 $_SESSION['grotti'] = $grotti;
+
+                //Carico le immagini.
+                $i = 0;
+                $immagini = null;
+                foreach($grotti_validati as $grotto){
+                    if($i == 0){
+                        $immagini = (new DBConnection)->getImages($grotto['id']);
+
+                    }else{
+                        array_push($immagini, (new DBConnection)->getImages($grotto['id']));
+                    }
+                    $i++;
+                }
+                $_SESSION['immagini'] = array_filter($immagini);
 
                 ViewLoader::load("_templates/header_admin");
                 ViewLoader::load("admin/index");
                 ViewLoader::load("_templates/footer");
             }elseif($_SESSION['user']['nome_ruolo'] == 'utente'){
+                //mostro l'index di base se l'utente non è admin.
                 ViewLoader::load("_templates/header_user");
                 ViewLoader::load("home/index");
                 ViewLoader::load("_templates/footer");
             }
         }else{
-            //mostro l'index di base
+            //mostro l'index di base se l'utente non è connesso.
             ViewLoader::load("_templates/header_base");
             ViewLoader::load("home/index");
             ViewLoader::load("_templates/footer");
         }
     }
 
+    /**
+     * Funzione che consente di passare alla pagina di modifica di un utente.
+     * @param $email string L'email dell'utente da modificare.
+     */
     public function updateUser($email){
-        echo "in<br>";
+        //Carico le classi che necessito.
         require_once "./application/models/DBConnection.php";
         require_once "./application/models/input_manager.php";
 
+        //Verifico che l'utente sia loggato e che sia un admin.
         if(isset($_SESSION['user'])) {
             if ($_SESSION['user']['nome_ruolo'] == 'admin') {
                 if(isset($_SESSION['users'])){
+                    //Cancello le sessioni se sono impostate così che la pagina di modifica mostri i dati corretti.
                     if(isset($_SESSION['utente'])){
                         unset($_SESSION['utente']);
                     }
                     if(isset($_SESSION['grotto'])){
                         unset($_SESSION['grotto']);
                     }
+                    //Se non è settata carico i ruoli che un utente può avere.
                     if(!isset($_SESSION['ruoli'])){
                         $ruoli = (new DBConnection)->getRuoli();
                         $_SESSION['ruoli'] = $ruoli;
                     }
 
-                    //Carico dal DB l'utente
+                    //Carico dal DB l'utente.
                     $im = new InputManager();
                     $email = filter_var($im->checkInput($email), FILTER_SANITIZE_EMAIL);
                     $utente = (new DBConnection)->getUser($email);
+
+                    //Verifico che l'utente esista.
                     if($utente == null){
+                        //Se non esiste resto alla pagina di admin.
                         header('Location: ' . URL . 'admin');
                         exit();
                     }
+                    //Se esiste passo alla pagina di modifica.
                     $_SESSION['utente'] = $utente;
                     header('Location: ' . URL . 'gestione');
                     exit();
@@ -73,32 +102,45 @@ class Admin
         }
     }
 
+    /**
+     * Funzione che consente di passare alla pagina di modifica di un grotto.
+     * @param $id int L'id del grotto da modificare.
+     */
     public function updateGrotto($id){
+        //Carico le classi che necessito.
         require_once "./application/models/DBConnection.php";
         require_once "./application/models/input_manager.php";
 
+        //Verifico che l'utente sia loggato e che sia un admin.
         if(isset($_SESSION['user'])) {
             if ($_SESSION['user']['nome_ruolo'] == 'admin') {
                 if(isset($_SESSION['grotti_validati'])){
+
+                    //Cancello le sessioni se sono impostate così che la pagina di modifica mostri i dati corretti.
                     if(isset($_SESSION['grotto'])){
                         unset($_SESSION['grotto']);
                     }
                     if(isset($_SESSION['utente'])){
                         unset($_SESSION['utente']);
                     }
+                    //Se non è settata carico le fascie di prezzo che un grotto può avere.
                     if(!isset($_SESSION['fasce_prezzo'])){
                         $fasce_prezzo = (new DBConnection)->getFascePrezzo();
                         $_SESSION['fasce_prezzo'] = $fasce_prezzo;
                     }
 
-                    //Carico dal DB il grotto
+                    //Carico dal DB il grotto.
                     $im = new InputManager();
                     $id = filter_var($im->checkInput($id), FILTER_SANITIZE_NUMBER_INT);
                     $grotto = (new DBConnection)->getGrotto($id);
+
+                    //Verifico che esista.
                     if($grotto == null){
+                        //Se non esiste resto alla pagina di admin.
                         header('Location: ' . URL . 'admin');
                         exit();
                     }
+                    //Se esiste passo alla pagina di modifica.
                     $_SESSION['grotto'] = $grotto;
                     header('Location: ' . URL . 'gestione');
                     exit();
--- codice/application/views/admin/index.php
+++ codice/application/views/admin/index.php
@@ -1,11 +1,16 @@
-<?php if(isset($_SESSION['user']) && $_SESSION['user']['nome_ruolo'] == 'admin' && isset($_SESSION['grotti']) && isset($_SESSION['grotti_validati']) && isset($_SESSION['users'])): ?>
+<?php if(isset($_SESSION['user']) && $_SESSION['user']['nome_ruolo'] == 'admin' && isset($_SESSION['grotti']) && isset($_SESSION['grotti_validati']) && isset($_SESSION['users']) && isset($_SESSION['immagini'])): ?>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class="my-5 text-center">
                     <h1 class="h1 d-block">Amministrazione grotti Ticino</h1>
                     <h6 class="h6">Connesso come: <?php echo $_SESSION['user']['nome'] . " " . $_SESSION['user']['cognome'] . " (" .$_SESSION['user']['username'] . ")" ?></h6>
-                    
+                    <ul class="list-group list-group-horizontal list-group-flush">
+                        <li class="list-group-item ml-auto"><a class="text-dark" href="#utenti">Utenti</a></li>
+                        <li class="list-group-item"><a class="text-dark" href="#grotti">Grotti</a></li>
+                        <li class="list-group-item"><a class="text-dark" href="#inserimenti">Inserimenti</a></li>
+                        <li class="list-group-item mr-auto"><a class="text-dark" href="#immagini">Immagini</a></li>
+                    </ul>
                     <?php
                         if(isset($_SESSION['errors'])){
                             foreach ($_SESSION['errors'] as $error) {
@@ -72,7 +77,7 @@
                             </thead>
                             <tbody>
                             <?php foreach ($_SESSION['grotti_validati'] as $row): ?>
-                                <tr class="text-center table-row" data-href="<?php echo URL; ?>admin/showGrotto/<?php echo $row['id']; ?>">
+                                <tr class="text-center table-row">
                                     <td><?php echo $row['nome']; ?></td>
                                     <td><?php echo($row['cap'] . " " . $row['paese'] . ", " .$row['via'] . " " . $row['no_civico']); ?></td>
                                     <td><?php echo $row['telefono']; ?></td>
@@ -114,7 +119,7 @@
                             </thead>
                             <tbody>
                             <?php foreach ($_SESSION['grotti'] as $row): ?>
-                                <tr class="text-center table-row" data-href="<?php echo URL; ?>admin/showGrotto/<?php echo $row['id']; ?>">
+                                <tr class="text-center table-row">
                                     <td><?php echo $row['nome']; ?></td>
                                     <td><?php echo($row['cap'] . " " . $row['paese'] . ", " .$row['via'] . " " . $row['no_civico']); ?></td>
                                     <td><?php echo $row['telefono']; ?></td>
@@ -126,6 +131,52 @@
                             </tbody>
                         </table>
                     </div>
+
+                    <!-- Sezione di gestione delle immagini -->
+                    <div class="my-5" id="immagini">
+                        <h3 class="h3-responsive">Gestione Immagini</h3>
+                        <table id="img_table" class="table table-hover table-responsive-md">
+                            <thead>
+                            <tr class="text-center">
+                                <th scope="col">ID</th>
+                                <th scope="col">Path</th>
+                                <th scope="col">Nome del Grotto</th>
+                                <th scope="col">Elimina</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($_SESSION['immagini'] as $row): ?>
+                                <tr class="text-center table-row click-row" data-href="<?php echo $row['path']; ?>">
+                                    <td><?php echo $row['id']; ?></td>
+                                    <td><?php echo $row['path']; ?></td>
+                                    <td><?php echo $_SESSION['grotti_validati'][array_search($row['grotto'], array_column($_SESSION['grotti_validati'], 'id'))]['nome']; ?></td>
+                                    <td><a type="button" class="btn btn-sm btn-danger" href="<?php echo URL; ?>gestione/elimina/immagine/<?php echo $row['id']; ?>">Elimina</a></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modale che mostra l'immagine selezionata -->
+    <div class="modal fade" id="img-modal" tabindex="-1" role="dialog" aria-labelledby="modal-label"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modal-label"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <img id="modal-body-img" src="">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
@@ -179,8 +230,22 @@
                     "sEmptyTable": "Nessun nuovo inserimento da mostrare"
                 }
             });
+            $('#img_table').DataTable({
+                "searching": false,
+                "bLengthChange": false,
+                "info" : false,
+                "iDisplayLength": 5,
+                "oLanguage": {
+                    "sEmptyTable": "Nessuna immagine da mostrare"
+                }
+            });
             $('.dataTables_length').addClass('bs-select');
 
+            $(".click-row").click(function() {
+                $('#modal-body-img').attr('src', $(this).data("href"));
+                $('#modal-label').attr('content', $(this).data("href"));
+                $('#img-modal').modal();
+            });
         });
     </script>
 <?php endif; ?>
